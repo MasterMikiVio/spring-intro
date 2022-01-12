e814db390b84dbc6522cd659f8abdd7e37e319d3
Mikołaj Wach <m.wach753@gmail.com>
2022-01-12 17:49:25 +0100

    Zadanie 1-7

--- src/main/java/com/example/MyNameMessageService.java
+++ src/main/java/com/example/MyNameMessageService.java
@@ -1,4 +1,10 @@
 package com.example;
 
 public class MyNameMessageService implements MessageService {
+    public String message = "Mikołaj Wach";
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
